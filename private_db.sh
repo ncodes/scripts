f411a3632ecdd9a2677bcae6db7c47069492bbd5
Kennedy Idialu <kennedyidialu@gmail.com>
2016-05-27 10:03:13 +0100

    increase range

--- private_db.sh
+++ private_db.sh
@@ -1,11 +1,11 @@
-sudo apt-get update
-sudo apt-get install iptables-persistent
+#sudo apt-get update
+#sudo apt-get install iptables-persistent
 
 sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT			
 sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT									
 sudo iptables -I INPUT 1 -i lo -j ACCEPT			
-sudo iptables -A INPUT -p tcp -m tcp -s 41.58.48.115,41.220.68.0/16 --dport 27017 -j ACCEPT
-sudo iptables -A INPUT -p tcp -m tcp -s 41.58.48.115,41.220.68.0/16 --dport 6379 -j ACCEPT								
+sudo iptables -A INPUT -p tcp -m tcp -s 41.58.48.115,41.220.68.0/24 --dport 27017 -j ACCEPT
+sudo iptables -A INPUT -p tcp -m tcp -s 41.58.48.115,41.220.68.0/24 --dport 6379 -j ACCEPT								
 sudo iptables -A INPUT -j DROP
 
 sudo invoke-rc.d iptables-persistent save			
