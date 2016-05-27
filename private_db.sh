b459175987aa044719e2ab8cc1fd5c82183013c6
Kennedy Idialu <kennedyidialu@gmail.com>
2016-05-27 10:39:00 +0100

    updated range

--- private_db.sh
+++ private_db.sh
@@ -4,8 +4,8 @@
 sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT			
 sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT									
 sudo iptables -I INPUT 1 -i lo -j ACCEPT			
-sudo iptables -A INPUT -p tcp -m tcp -s 41.58.48.115,197.210.0.0/32 --dport 27017 -j ACCEPT
-sudo iptables -A INPUT -p tcp -m tcp -s 41.58.48.115,41.220.68.0/32 --dport 6379 -j ACCEPT								
+sudo iptables -A INPUT -p tcp -m tcp -s 41.58.48.115,197.210.0.0/16 --dport 27017 -j ACCEPT
+sudo iptables -A INPUT -p tcp -m tcp -s 41.58.48.115,197.210.0.0/16 --dport 6379 -j ACCEPT								
 sudo iptables -A INPUT -j DROP
 
-sudo invoke-rc.d iptables-persistent save			
+sudo invoke-rc.d iptables-persistent save			 
